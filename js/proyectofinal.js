69779960ab0d2118ab65c714f18697b08df5d1d8
Sebastian Gomez <sebasgomez99@gmail.com>
2024-10-05 06:50:29 -0500

    entregas

--- js/proyectofinal.js
+++ js/proyectofinal.js
@@ -1,39 +1,44 @@
 /**
  * *1. Tengo que traerme todos los inputs para poder que se agreguen al historial
- * *2.Tengo que agregar el fecth para que me traiga los telefonos y se agreguen en el medio de todo mi contenedor 
- * *3.  Agergar un boton que le de al body una clase que haga que se muestre   el recuadro con los objetos necesarios 
+ * *2.Tengo que agregar el fecth para que me traiga los telefonos y se agreguen en el medio de todo mi contenedor
+ * *3.  Agergar un boton que le de al body una clase que haga que se muestre   el recuadro con los objetos necesarios
  * *4. Agregar los productor con un inner html
- * *5. 
+ * *5.
  */
 
-//*Traer todos los inputs y formulario 
+//*Traer todos los inputs y formulario
 
-const form = document.querySelector('#registro');
-const nombreInput = document.querySelector('#nombre');
-const telefonoInput = document.querySelector('#telefono');
-const paisinput = document.querySelector('#pais');
-const correoInput = document.querySelector('#correo');
+const form = document.querySelector("#registro");
+const nombreInput = document.querySelector("#nombre");
+const telefonoInput = document.querySelector("#telefono");
+const paisinput = document.querySelector("#pais");
+const correoInput = document.querySelector("#correo");
 
 //*Historial Vacio
 let historial = [];
 
 //*Evento de submit para el formulario
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if(!isNaN(nombreInput.value)){
-        nombreInput.value = '';
+    Swal.fire({
+        title: "Iinformación Agregada con Éxito",
+        text: "Presiona OK para seguir utilizando la plataforma",
+        icon: "success",
+    });
+    if (!isNaN(nombreInput.value)) {
+        nombreInput.value = "";
         nombreInput.focus();
-        return
+        return;
     }
-    if(isNaN(telefonoInput.value) || (telefonoInput.value.lenght) < 10){
-        telefonoInput.value = '';
+    if (isNaN(telefonoInput.value) || telefonoInput.value.lenght < 10) {
+        telefonoInput.value = "";
         telefonoInput.focus();
-        return
+        return;
     }
-    if(!isNaN(paisinput.value)){
-        paisinput.value = '';
+    if (!isNaN(paisinput.value)) {
+        paisinput.value = "";
         paisinput.focus();
-        return
+        return;
     }
     const construccion = constructor();
     agregarHistorial(construccion);
@@ -41,83 +46,108 @@ form.addEventListener('submit', (e) => {
     agregarLocalStorage();
     form.reset();
     nombreInput.focus();
-})
+});
 
 //*Funcion constructora
-function constructor (){
+function constructor() {
     let nombre = nombreInput.value;
     let telefono = telefonoInput.value;
     let pais = paisinput.value;
     let correo = correoInput.value;
-    return {nombre, telefono, pais, correo}
+    return { nombre, telefono, pais, correo };
 }
 
-//* Funcion agregar al historial 
-function agregarHistorial (producto){
-    historial.push(producto)
+//* Funcion agregar al historial
+function agregarHistorial(producto) {
+    historial.push(producto);
 }
 
-//*Traer la lista 
-const lista = document.querySelector('#lista-historial');
+//*Traer la lista
+const lista = document.querySelector("#lista-historial");
 
-//*Crear el elemento y agregarlo al historial para mostrarlo 
-function mostrarHistorial (){
-    lista.innerHTML = '';
-historial.forEach((historia, index) => {
-    let li = document.createElement('li');
-    li.innerText = `Producto # ${index + 1}
-Nombre : ${historia.nombre}
-Precio: ${historia.telefono}
-Descuento: ${historia.pais}
-Correo Electronico: ${historia.correo}`
-lista.appendChild(li)
-})
+//*Crear el elemento y agregarlo al historial para mostrarlo
+function mostrarHistorial() {
+    lista.innerHTML = "";
+    historial.forEach((historia, index) => {
+        let li = document.createElement("li");
+        li.innerHTML = `Persona # ${index + 1} <br>
+<strong>Nombre :</strong> ${historia.nombre} <br>
+<strong>Telefono:</strong> ${historia.telefono} <br>
+<strong>Pais:</strong> ${historia.pais} <br>
+<strong>Correo Electronico:</strong> ${historia.correo}`;
+        lista.appendChild(li);
+    });
 }
 
-//* Agregar historial al storage 
-function agregarLocalStorage (){
-    localStorage.setItem('historial', JSON.stringify(historial));
+//* Agregar historial al storage
+function agregarLocalStorage() {
+    localStorage.setItem("historial", JSON.stringify(historial));
 }
 //*Traer todo del local storage
-function historialEnLocalStorage(){
-    const historialLS = JSON.parse(localStorage.getItem('historial'));
-    if (historialLS && historialLS.lenght != 0) {  
-        historialLS.forEach(producto => {
-            agregarHistorial(producto);  
+function historialEnLocalStorage() {
+    const historialLS = JSON.parse(localStorage.getItem("historial"));
+    if (historialLS && historialLS.lenght != 0) {
+        historialLS.forEach((producto) => {
+            agregarHistorial(producto);
         });
-        mostrarHistorial();  
+        mostrarHistorial();
     }
 
-    nombreInput.focus();  
+    nombreInput.focus();
 }
 historialEnLocalStorage();
 
-//* Traer el contenedor para poder pintar las tarjetas 
+//* Traer el contenedor para poder pintar las tarjetas
 
-const contenedor = document.querySelector('#contenedor-productos');
+const contenedor = document.querySelector("#contenedor-productos");
 
-//* Funcion para pintar los productos en el contenedor 
-function mostrarProductos (arr){
-contenedor.innerHTML = '';
-let htmlInsertado;
-for (const elemento of arr) {
-    const {img, id, nombre, precio} = elemento
-    htmlInsertado= `<div class = "card">
-    <img src = "../img/${img}" alt = " ${nombre}">
-    <hr>
-    <h3>${nombre}</h3>
-    <p class = "parrafo-precio">Precio $:  ${precio.toLocaleString('es-Es')}</p>
-    <div class = "card-action">
-    <button class =" btn btn success" id = "${id}">Agregar</button>
-    </div>
-    </div>`
-contenedor.innerHTML += htmlInsertado
+//* Funcion para pintar los productos en el contenedor
+function mostrarProductos(arr) {
+    contenedor.innerHTML = "";
+    let htmlInsertado;
+    if (Array.isArray(arr) && arr.length > 0) {
+        for (const elemento of arr) {
+            const { img, id, nombre, precio } = elemento;
+            htmlInsertado = `<div class="card">
+            <img src="../img/${img}" alt="${nombre}">
+            <hr>
+            <h3>${nombre}</h3>
+            <p class="parrafo-precio">Precio $: ${precio.toLocaleString(
+                "es-Es"
+            )}</p>
+            <div class="card-action">
+              <button class="btn btn-success agregar-producto" id="${id}">Agregar</button>
+            </div>
+          </div>`;
+            contenedor.innerHTML += htmlInsertado;
+        }
+
+        // Seleccionamos todos los botones generados dinámicamente
+        const botones = document.querySelectorAll(".agregar-producto");
+
+        // Iteramos sobre cada botón para agregar el evento click
+        botones.forEach((boton) => {
+            boton.addEventListener("click", () => {
+                Swal.fire({
+                    title: "Producto Agregado con Éxito",
+                    icon: "success",
+                    text: `El producto con ID ${boton.id} fue agregado al carrito.`,
+                });
+            });
+        });
+    } else {
+        htmlInsertado = `<div class="contenedor-no-resultados">
+          <h2 class="no-resultados">No hay resultados</h2>
+        </div>`;
+        contenedor.innerHTML += htmlInsertado;
+    }
 }
-}
 
-
-fetch('../data/telefonos.json')
-.then((response) => response.json())
-.then((servicio) => {
-mostrarProductos(servicio)
-})
+fetch("../data/telefonos.json")
+    .then((response) => response.json())
+    .then((servicio) => {
+        mostrarProductos(servicio);
+    })
+    .catch((error) => {
+        mostrarProductos(error);
+    });
---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from './components/NavBar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <NavBar  />
+    </div>
+  )
+}
+
+export default App
